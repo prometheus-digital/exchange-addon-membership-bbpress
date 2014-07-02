b2221f052743838151e890dbd78032545f624387
Lew Ayotte <lew@ithemes.com>
2014-07-02 16:17:40 -0500

    Added filter to make sure bbPress content is listed in member  dashboard

--- lib/required-hooks.php
+++ lib/required-hooks.php
@@ -100,3 +100,49 @@ function it_exchange_membership_bbpress_get_template_stack( $stack ) {
 	return $stack;
 }
 add_filter( 'bbp_get_template_stack', 'it_exchange_membership_bbpress_get_template_stack', 10 );
+
+/**
+ * This adds the bbPress restricted content to the Membership Dashboard
+ *
+ * @since 1.0.0
+ *
+ * @return void
+*/
+function it_exchange_membership_bbpress_addon_membership_content_restricted_posts( $restricted_posts, $selection, $selected, $value ) {
+
+	if ( empty( $restricted_posts ) ) {
+	
+		//We need to do this because bbPress sets exclude_from_search for their post types.
+	
+		switch ( $selection ) {
+			
+			case 'forum':
+				$args = array(
+					'post_type' => apply_filters( 'bbp_forum_post_type', 'forum' ),
+					'p'   => $value,
+				);
+				$restricted_posts = get_posts( $args );
+				break;
+			case 'topic':
+				$args = array(
+					'post_type' => apply_filters( 'bbp_topic_post_type', 'topic' ),
+					'p'   => $value,
+				);
+				$restricted_posts = get_posts( $args );
+				break;
+			case 'reply':
+				$args = array(
+					'post_type' => apply_filters( 'bbp_reply_post_type', 'reply' ),
+					'p'   => $value,
+				);
+				$restricted_posts = get_posts( $args );
+				break;
+			
+		}
+		
+	}
+
+	return $restricted_posts;
+	
+}
+add_filter( 'it_exchange_membership_addon_membership_content_restricted_posts', 'it_exchange_membership_bbpress_addon_membership_content_restricted_posts', 10, 4 );
