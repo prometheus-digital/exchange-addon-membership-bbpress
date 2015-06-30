aea10e81d5b86afcb3ab93c9ba93c21315229f77
Lew Ayotte <lew@ithemes.com>
2015-06-30 07:28:41 -0500

    Tagging 1.1.0

--- exchange-addon-membership-bbpress.php
+++ exchange-addon-membership-bbpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Membership bbPress Add-on
- * Version: 1.0.4
+ * Version: 1.1.0
  * Description: Adds the iThemes Exchange Membership management functionality to bbPress
  * Plugin URI: http://ithemes.com/exchange/membership-bbpress/
  * Author: iThemes
